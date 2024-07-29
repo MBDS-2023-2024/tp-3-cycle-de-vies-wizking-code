fefbd79a400221bdd1e8b619ee4ea4f6a08cc5d4
Jeffking Sterile <jsterile@solutions.ht>
2024-07-29 01:19:56 -0400

    Tâche 1: Effecutuer une opération mathématique simple

--- app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/ComputeActivity.kt
@@ -1,15 +1,33 @@
 package fr.gobelins.dmi1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import fr.gobelins.dmi1.databinding.ActivityMainBinding
+import fr.gobelins.dmi1.databinding.ComputeActivityBinding
 
 class ComputeActivity : AppCompatActivity() {
+    private  lateinit var binding: ComputeActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.compute_activity)
+        binding = ComputeActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val operation = intent.getStringExtra("operation") ?: "ADD"
+        binding.btnCompute.setOnClickListener {
+           add()
+        }
+    }
 
+    private fun add(){
+        val firstValue = binding.firstOperand.text.toString()
+        val secondValue = binding.secondOperand.text.toString()
 
+        if (!firstValue.isNullOrEmpty() && !secondValue.isNullOrEmpty()) {
+            val resultOperand = firstValue.toBigDecimal() + secondValue.toBigDecimal()
+             binding.txtResult.text = resultOperand.toString()
+        } else {
+            Toast.makeText(this ,"Veuillez entrer des nombres", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
+++ app/src/main/java/fr/gobelins/dmi1/MainActivity.kt
@@ -1,11 +1,20 @@
 package fr.gobelins.dmi1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import fr.gobelins.dmi1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnHomeCompute.setOnClickListener{
+            val intent = Intent(this, ComputeActivity::class.java)
+            intent.extras?.putString("operation", "ADD")
+            startActivity(intent)
+        }
     }
 }
